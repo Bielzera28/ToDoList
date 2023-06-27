836880615078cee1ba41aa087b2ba8dec2e3f48e
Gabriel Aguiar <aguiar.desenvolvedor@gmail.com>
2023-06-27 13:52:07 -0300

    Lógica para envio de formulário

--- src/components/todoForm/TodoForm.jsx
+++ src/components/todoForm/TodoForm.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import { useState } from "react";
 
 const TodoForm = () => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value || !category) return;
+    setValue("");
+    setCategory("");
+  };
   return (
     <div className="todo-form">
-        <h2>Criar Tarefa:</h2>
-        <form>
-            <input type="text" placeholder="Digite o título"/>
-            <select>
-                <option value="">Selecione uma Categoria</option>
-                <option value="Trabalho">Trabalho</option>
-                <option value="Pessoal">Pessoal</option>
-                <option value="Estudos">Estudos</option>
-            </select>
-            <button type="submit">Criar Tarefa</button>
-        </form>
-
+      <h2>Criar Tarefa:</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o título"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select onChange={(e) => setCategory(e.target.value)} value={category}>
+          <option value="">Selecione uma Categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Pessoal">Pessoal</option>
+          <option value="Estudos">Estudos</option>
+        </select>
+        <button type="submit">Criar Tarefa</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default TodoForm
+export default TodoForm;
